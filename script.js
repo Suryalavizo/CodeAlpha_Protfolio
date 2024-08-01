a37148548d72292a8ef4c338d9df9fcdc1d136d8
Suryalavizo <suryasiva3399@gmail.com>
2024-08-01 22:46:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+document.querySelectorAll('nav a').forEach((link) => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const targetId = link.getAttribute('href').substring(1);
+        document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
+    });
+});
